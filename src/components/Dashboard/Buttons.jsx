da4f8c506214fb452eae0f8ed82d1f7e91705bb8
Ashton <ashtonhowa@gmail.com>
2025-10-16 12:02:23 -0400

    tina cms for buttons

--- src/components/Dashboard/Buttons.jsx
+++ src/components/Dashboard/Buttons.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link"
+import { tinaField } from "tinacms/dist/react"
 
 function Buttons({buttons}){
     return(
         <div className="flex justify-center">
            {buttons?.map((button,i) => (
                 <div key={i}>
-                    {button.style == 'border' && (<Link href={button.link}><button className="px-8 capitalize py-2 border primary-border rounded hover:text-white/80 transition-colors duration-300">{button.label}</button></Link>)}
-                    {button.style == 'button' && (<Link href={button.link}><button className="bg-primary capitalize cursor-pointer px-8 py-2 w-auto rounded hover:opacity-80 text-white ">{button.label}</button></Link>)}
+                    {button.style == 'border' && (<Link href={button.link}><button data-tina-field={tinaField(button,'label')} className="px-8 capitalize py-2 border primary-border rounded hover:text-white/80 transition-colors duration-300">{button.label}</button></Link>)}
+                    {button.style == 'button' && (<Link href={button.link}><button data-tina-field={tinaField(button,'label')} className="bg-primary capitalize cursor-pointer px-8 py-2 w-auto rounded hover:opacity-80 text-white ">{button.label}</button></Link>)}
                 </div>
            ))}
         </div>
