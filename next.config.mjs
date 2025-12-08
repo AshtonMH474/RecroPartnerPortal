fceeffe0fef5f918aeb81c9e9ae42a03ae1fd61e
Ashton Howard <ashtonhowa@gmail.com>
2025-12-08 11:42:34 -0500

    updated react and nextjs for security

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-        });
-        return config;
-    },
+  // 👇 Enable Turbopack explicitly
+  turbopack: {},
+
   reactStrictMode: true,
-  async rewrites(){
-        return [
-            {
-                source:'/admin',
-                destination:'/admin/index.html'
-            }
-        ]
-    }
+
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
