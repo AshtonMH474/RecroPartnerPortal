952f76d708c1493afc8b2b4d34d51f8f20cfdb1d
Ashton <ashtonhowa@gmail.com>
2026-01-07 14:17:54 -0500

    feat(contact-form): show inline success message instead of alert

--- src/pages/api/hubspot/post-ticket.js
+++ src/pages/api/hubspot/post-ticket.js
@@ -196,7 +196,7 @@ async function handler(req, res) {
       createdAt: new Date(),
     });
 
-    return res.status(200).json({
+    return res.status(201).json({
       success: true,
       message: "Opportunity created and associated with contact",
     });
